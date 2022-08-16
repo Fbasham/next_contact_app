7a7527e6f5549dafac5d1723fd8aaf4360f84964
Frank <frankabasham@gmail.com>
2022-08-16 10:58:52 -0400

    add SSG

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,24 @@
+import Link from 'next/link'
 import { AiOutlineMail, AiOutlinePhone, AiOutlineUser } from 'react-icons/ai'
 
 export default function Card({client}){
   console.log(client)
-  return <div className="flex flex-col gap-3 p-2 text-white rounded-lg shadow-lg bg-slate-500">
-    <div className='flex items-center gap-2'>
-      <AiOutlineUser />
-      <p>{client.name}</p>
-    </div>
-    <div className='flex items-center gap-2'>
-      <AiOutlineMail />
-      <p className='text-xs'>{client.email}</p>
-    </div>
-    <div className='flex items-center gap-2'>
-      <AiOutlinePhone />
-      <p className="text-xs">{client.phone}</p>
-    </div>
+  return <div className="flex flex-col gap-3 p-2 text-white rounded-lg shadow-lg cursor-pointer bg-slate-500 hover:bg-slate-700">
+    <Link href={`http://localhost:3000/client/${client.id}`}>
+      <div>
+        <div className='flex items-center gap-2'>
+          <AiOutlineUser />
+          <p>{client.name}</p>
+        </div>
+        <div className='flex items-center gap-2'>
+          <AiOutlineMail />
+          <p className='text-xs'>{client.email}</p>
+        </div>
+        <div className='flex items-center gap-2'>
+          <AiOutlinePhone />
+          <p className="text-xs">{client.phone}</p>
+        </div>
+      </div>
+    </Link>
   </div>
 }
---
+++ pages/client/[id].js
@@ -0,0 +1,48 @@
+export default function Client({ client }){
+  return <div className="flex flex-col gap-2 m-5">
+    <h1 className="text-4xl">{client.name}</h1>
+    <h2 className="text-2xl text-pink-500">Contact Information</h2>
+    <dl>
+      <dt className="font-semibold text-md">Email</dt>
+      <dd className="text-sm">{client.email}</dd>
+      <dt className="font-semibold text-md">Phone</dt>
+      <dd className="text-sm">{client.phone}</dd>
+      <dt className="font-semibold text-md">Website</dt>
+      <dd className="text-sm">{client.website}</dd>
+    </dl>
+    <h2 className="text-2xl text-pink-500">Address</h2>
+    <dl>
+      <dt className="font-semibold text-md">Street</dt>
+      <dd className="text-sm">{client.address.street}</dd>
+      <dt className="font-semibold text-md">Suite</dt>
+      <dd className="text-sm">{client.address.suite}</dd>
+      <dt className="font-semibold text-md">City</dt>
+      <dd className="text-sm">{client.address.city}</dd>
+    </dl>
+    <h2 className="text-2xl text-pink-500">Company</h2>
+    <dl>
+      <dt className="font-semibold text-md">Name</dt>
+      <dd className="text-sm">{client.company.name}</dd>
+      <dt className="font-semibold text-md">Catch Phrase</dt>
+      <dd className="text-sm">{client.company.catchPhrase}</dd>
+      <dt className="font-semibold text-md">Mandate</dt>
+      <dd className="text-sm">{client.company.bs}</dd>
+    </dl>
+  </div>
+}
+
+export async function getStaticPaths(){
+  let clients = await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+  return {
+    paths: clients.map(client=>({params: { id: client.id+'' }})),
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context){
+  const { id } = context.params
+  let client = await (await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)).json()
+  return {
+    props: { client }
+  }
+}
