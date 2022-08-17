e415ae0a2e21549d7cd670d6a657ba75b3f46186
Frank <frankabasham@gmail.com>
2022-08-17 07:38:33 -0400

    made changes to allow for keyboard accessibility

--- components/Card.js
+++ components/Card.js
@@ -3,10 +3,10 @@ import Link from 'next/link'
 import { AiOutlineMail, AiOutlinePhone, AiOutlineUser } from 'react-icons/ai'
 
 export default function Card({client,image}){
-  return <div className="flex items-center justify-between px-4 py-2 text-white duration-200 rounded-lg shadow-lg cursor-pointer bg-slate-500 hover:bg-slate-700">
-    <div>
-      <Link href={`/client/${client.id}`}>
-        <a className='flex flex-col gap-1 p-1'>
+  return <div className="text-white duration-200 rounded-lg shadow-lg cursor-pointer bg-slate-500 hover:bg-slate-700">
+    <Link href={`/client/${client.id}`}>
+      <a className='flex items-center justify-between px-4 py-2'>
+        <div className='flex flex-col gap-1 p-1'>
           <div className='flex items-center gap-2 text-sm'>
             <AiOutlineUser />
             <p>{client.name}</p>
@@ -19,16 +19,16 @@ export default function Card({client,image}){
             <AiOutlinePhone />
             <p className="text-xs">{client.phone}</p>
           </div>
-        </a>
-      </Link>
-    </div>
-    <div className='relative w-24 h-24'>
-      <Image 
-        src={image}
-        layout='fill'
-        objectFit='cover'
-        className='rounded-full'
-      />
-    </div>
+        </div>
+        <div className='relative w-24 h-24'>
+          <Image 
+            src={image}
+            layout='fill'
+            objectFit='cover'
+            className='rounded-full'
+            />
+        </div>
+      </a>
+    </Link>
   </div>
 }
