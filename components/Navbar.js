91ce47bd1408945329cbe2b7f25e0b8fe0c180c3
Frank <frankabasham@gmail.com>
2022-08-17 07:41:04 -0400

    made changes to allow for keyboard accessibility

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,9 @@ export default function Navbar(){
   return <nav className="flex items-center justify-between h-12 text-lg font-semibold text-white bg-slate-500">
     <div className="h-full px-4 pt-[12px] text-2xl duration-200 cursor-pointer hover:bg-slate-700">
       <Link href='/'>
-        <AiOutlineHome/>
+        <a>
+          <AiOutlineHome/>
+        </a>
       </Link>
     </div>
     <div>
@@ -13,16 +15,20 @@ export default function Navbar(){
     </div>
     <div className="flex h-full">
       <Link href='/about'>
-        <div className="flex items-center gap-2 p-2 duration-200 cursor-pointer hover:bg-slate-700">
-          <AiOutlineQuestionCircle />
-          <div>About</div>
-        </div>
+        <a>
+          <div className="flex items-center gap-2 p-2 duration-200 cursor-pointer hover:bg-slate-700">
+            <AiOutlineQuestionCircle />
+            <div>About</div>
+          </div>
+        </a>
       </Link>
       <Link href='/contact'>
-        <div className="flex items-center gap-2 p-2 duration-200 cursor-pointer hover:bg-slate-700">
-          <AiOutlineContacts />
-          <div>Contact</div>
-        </div>
+        <a>
+          <div className="flex items-center gap-2 p-2 duration-200 cursor-pointer hover:bg-slate-700">
+            <AiOutlineContacts />
+            <div>Contact</div>
+          </div>
+        </a>
       </Link>
     </div>
   </nav>
